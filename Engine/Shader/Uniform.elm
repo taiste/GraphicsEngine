3f43bf10fa50c3ff18f0fdebdad30bc6e6b3ffbc
Timo Vesalainen <timo@taiste.fi>
2017-11-01 13:03:47 +0200

    Updated uniform to match what is actually used by new shaders

--- Engine/Shader/Uniform.elm
+++ Engine/Shader/Uniform.elm
@@ -1,4 +1,4 @@
-module Engine.Shader.Uniform exposing (Uniform, constructUniform)
+module Engine.Shader.Uniform exposing (Uniform)
 
 {-| This module contains the definition of the Uniform type and
 a function to construct a uniform from a scene and a renderable object
@@ -15,63 +15,13 @@ import Math.Vector3 exposing (Vec3)
 import Math.Matrix4 exposing (Mat4)
 import Engine.Render.Renderable exposing (Renderable)
 import Engine.Scene.Scene exposing (Scene)
-import Engine.Math.Utils exposing (
-  modelMatrix,
-  viewMatrix,
-  projectionMatrix,
-  modelViewMatrix,
-  modelViewProjectionMatrix,
-  normalMatrix)
 
 
--- TODO: Find a different strategy for dealing with Uniforms
---       -- Note, only the first three are truly necessary
---       -- Consider joining the model and view matrices into a modelViewMatrix
-{-| Container type to hold all of the properties that are passed to GLSL
-as uniforms.
--}
 type alias Uniform = {
-  modelMatrix               : Mat4,
-  viewMatrix                : Mat4,
-  projectionMatrix          : Mat4,
-  modelViewMatrix           : Mat4,
+  light : Light,
+  viewMatrix : Mat4,
   modelViewProjectionMatrix : Mat4,
-  normalMatrix              : Mat4,
-  lightPosition             : Vec3,
-  lightRotation             : Vec3,
-  lightColor                : Vec3,
-  lightIntensity            : Float,
-  materialEmissiveColor     : Vec3,
-  materialEmissiveStrength  : Float,
-  materialAmbientColor      : Vec3,
-  materialAmbientStrength   : Float,
-  materialDiffuseColor      : Vec3,
-  materialDiffuseStrength   : Float,
-  materialSpecularColor     : Vec3,
-  materialSpecularStrength  : Float
+  modelViewMatrix : Mat4,
+  material : MaterialValues,
+  normalMatrix : Mat4
 }
-
-
-{-| Constructs a record of type Uniform from a scene and a renderable object
-to be sent to GLSL as uniforms.
--}
-constructUniform : Scene -> Renderable -> Uniform
-constructUniform scene object = {
-  modelMatrix               = modelMatrix object,
-  viewMatrix                = viewMatrix scene.camera,
-  projectionMatrix          = projectionMatrix scene.camera,
-  modelViewMatrix           = modelViewMatrix object scene.camera,
-  modelViewProjectionMatrix = modelViewProjectionMatrix object scene.camera,
-  normalMatrix              = normalMatrix object scene.camera,
-  lightPosition             = scene.light.position,
-  lightRotation             = scene.light.rotation,
-  lightColor                = scene.light.color,
-  lightIntensity            = scene.light.intensity,
-  materialEmissiveColor     = object.material.emissive.color,
-  materialEmissiveStrength  = object.material.emissive.strength,
-  materialAmbientColor      = object.material.ambient.color,
-  materialAmbientStrength   = object.material.ambient.strength,
-  materialDiffuseColor      = object.material.diffuse.color,
-  materialDiffuseStrength   = object.material.diffuse.strength,
-  materialSpecularColor     = object.material.specular.color,
-  materialSpecularStrength  = object.material.specular.strength }
